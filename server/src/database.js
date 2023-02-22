668140410da4f30b80a99823480272399c4af996
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-02-21 20:20:24 -0800

    updated TODOs

--- server/src/database.js
+++ server/src/database.js
@@ -12,20 +12,38 @@ export const db = mysql.createPool({
 
 await db.query("DROP TABLE IF EXISTS COUNTRY, OCEAN, ATMOSPHEREICDATA, LANDDATA, ECONOMICDATA, POLITICALDATA, OCEANDATA");
 
-// TODO: Create COUNTRY table and define attributes, domains, and constraints (do not implement foreign keys yet)
+// TODO: Create COUNTRY table and define attributes, domains, and constraints
 await db.query(`
 CREATE TABLE COUNTRY 
 (countryName VARCHAR(255) PRIMARY KEY, countryPopulation INT)
 `);
 
-// TODO: Create OCEAN table and define attributes, domains, and constraints (do not implement foreign keys yet)
+// TODO: Create OCEAN table and define attributes, domains, and constraints
+// await db.query(`
 
-// TODO: Create ATMOSPHERICDATA table and define attributes, domains, and constraints (do not implement foreign keys yet)
+// `);
 
-// TODO: Create LANDDATA table and define attributes, domains, and constraints (do not implement foreign keys yet)
+// TODO: Create ATMOSPHERICDATA table and define attributes, domains, and constraints
+// await db.query(`
 
-// TODO: Create ECONOMICDATA table and define attributes, domains, and constraints (do not implement foreign keys yet)
+// `);
 
-// TODO: Create POLITICALDATA table and define attributes, domains, and constraints (do not implement foreign keys yet)
+// TODO: Create LANDDATA table and define attributes, domains, and constraints
+// await db.query(`
 
-// TODO: Create OCEANDATA table and define attributes, domains, and constraints (do not implement foreign keys yet)
+// `);
+
+// TODO: Create ECONOMICDATA table and define attributes, domains, and constraints
+// await db.query(`
+
+// `);
+
+// TODO: Create POLITICALDATA table and define attributes, domains, and constraints
+// await db.query(`
+
+// `);
+
+// TODO: Create OCEANDATA table and define attributes, domains, and constraints
+// await db.query(`
+
+// `);
