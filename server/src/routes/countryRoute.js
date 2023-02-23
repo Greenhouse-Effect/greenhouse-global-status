05bbc39765d6d09981d8e587e42cc8c17b6c8bc3
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-02-23 15:14:41 -0800

    added get route for countries by population and populationChange

--- server/src/routes/countryRoute.js
+++ server/src/routes/countryRoute.js
@@ -3,32 +3,84 @@ import { getCountries, getCountry, insertCountry } from '../queries/countryQuery
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const countries = await getCountries();
-    res.status(200).send(countries);
-  } catch(err) {
-    res.status(500).json({ message: err.message })
-  }
-});
-
-router.get("/:name", async (req, res) => {
-  try {
-    const name = req.params.name;
-    const country = await getCountry(name);
-    res.send(country);
-  } catch(err) {
-    res.status(500).json({ message: err.message })
-  }
-});
-
+/**
+ * Route creates a new country based on properties from request body and stores it into db
+ * 
+ * @param request - HTTP request. Contains name, population in body.
+ * @param response - HTTP response. Responds with posted country and status code based on functionality of route.
+ */
 router.post("/", async (req, res) => {
   try {
     const { name, population } = req.body;
     const country = await insertCountry(name, population);
     res.status(201).send(country);
   } catch(err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Route gets all countries from db
+ * 
+ * @param request - HTTP request.
+ * @param response - HTTP response. Responds with all countries and status code based on functionality of route.
+ */
+router.get("/", async (req, res) => {
+  try {
+    const countries = await getCountries();
+    res.status(200).send(countries);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Route gets one country based on name property from request body
+ * 
+ * @param request - HTTP request. Contains name in params.
+ * @param response - HTTP response. Responds with one country and status code based on functionality of route.
+ */
+router.get("/:name", async (req, res) => {
+  try {
+    const name = req.params.name;
+    const country = await getCountry(name);
+    res.status(200).send(country);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Route gets all countries based on population property from request body
+ * 
+ * @param request - HTTP request. Contains population in params.
+ * @param response - HTTP response. Responds with countries whose population are greater than the request param and 
+ *                  status code based on functionality of route.
+ */
+router.get("/:population", async (req, res) => {
+  try {
+    const population = req.params.population;
+    const countries = 1; // use function from countryQueries
+    res.status(200).send(countries);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Route gets all countries based on population property from request body
+ * 
+ * @param request - HTTP request. Contains population yearly change in params.
+ * @param response - HTTP response. Responds with countries whose population yearly change are greater than the request param and 
+ *                  status code based on functionality of route.
+ */
+router.get("/:populationChange", (req, res) => {
+  try {
+    const populationChange = req.params.populationChange;
+    const countries = 1; // use function from countryQueries
+    res.status(200).send(countries);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
