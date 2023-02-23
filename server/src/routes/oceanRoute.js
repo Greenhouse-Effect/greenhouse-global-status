99b7bf17b8c0f02ab321281cbf960ccde290093c
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-02-23 15:14:59 -0800

    added post and get routes for ocean

---
+++ server/src/routes/oceanRoute.js
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/**
+ * Route creates a new ocean based on properties from request body and stores it into db
+ * 
+ * @param request - HTTP request. Contains name in body.
+ * @param response - HTTP response. Responds with posted ocean and status code based on functionality of route.
+ */
+router.post("/", async (req, res) => {
+  try {
+    const { name } = req.body;
+    const ocean = 1; // use function from oceanQuery
+    res.status(201).send(ocean);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Route gets all oceans from db
+ * 
+ * @param request - HTTP request.
+ * @param response - HTTP response. Responds with all oceans and status code based on functionality of route.
+ */
+router.get("/", async (req, res) => {
+  try {
+    const oceans = 1; // use function from oceanQuery
+    res.status(200).send(oceans);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Route gets one ocean based on name property from request body
+ * 
+ * @param request - HTTP request. Contains name in params.
+ * @param response - HTTP response. Responds with one ocean and status code based on functionality of route.
+ */
+router.get("/:name", async (req, res) => {
+  try {
+    const name = req.params.name;
+    const ocean = 1; // use function from oceanQuery
+    res.status(200).send(ocean);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
