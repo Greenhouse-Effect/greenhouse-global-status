71f6e07932fa0bcdabd8df81ba884cc2bf5e3deb
DanielBKurt <danielbkurt@gmail.com>
2023-03-02 14:42:13 -0800

    Updated tables

--- server/src/database.js
+++ server/src/database.js
@@ -10,9 +10,11 @@ export const db = mysql.createPool({
 }).promise();
 
 
-await db.query("DROP TABLE IF EXISTS COUNTRY, OCEAN, ATMOSPHERICDATA, LANDDATA, ECONOMICDATA, POLITICALDATA, OCEANDATA");
+// leaving old tables in for now to make sure they are dropped
+await db.query("DROP TABLE IF EXISTS COUNTRY, OCEAN, ATMOSPHERICDATA, LANDDATA, ECONOMICDATA, POLITICALDATA, OCEANDATA, SOCIETALDATA, ENERGYDATA");
 
 // TODO: Create COUNTRY table and define attributes, domains, and constraints
+// TODO: Add population change (not doing that yet so that queries and routes fully work)
 await db.query(`
 CREATE TABLE COUNTRY 
 (countryName VARCHAR(255) PRIMARY KEY, countryPopulation INT)
@@ -27,23 +29,25 @@ CREATE TABLE OCEAN
 // TODO: Create ATMOSPHERICDATA table and define attributes, domains, and constraints
 await db.query(`
 CREATE TABLE ATMOSPHERICDATA
-(countryName VARCHAR(255) NOT NULL, year INT NOT NULL, emissionsLevel INT, tempHigh INT, tempLow INT, PRIMARY KEY (countryName, year))
+(countryName VARCHAR(255) NOT NULL, year INT NOT NULL, emissionsLevel INT, tempHigh INT, tempLow INT, units INT, PRIMARY KEY (countryName, year))
 `);
 
 // TODO: Create LANDDATA table and define attributes, domains, and constraints
-// await db.query(`
-
-// `);
-
-// TODO: Create ECONOMICDATA table and define attributes, domains, and constraints
-// await db.query(`
-
-// `);
-
-// TODO: Create POLITICALDATA table and define attributes, domains, and constraints
 await db.query(`
-CREATE TABLE POLITICALDATA
-(countryName VARCHAR(255) NOT NULL, year INT NOT NULL, sdg INT, hdi INT, PRIMARY KEY (countryName, year))
+CREATE TABLE LANDDATA
+(countryName VARCHAR(255) NOT NULL, year INT NOT NULL, landArea INT, agEmissions INT, agWater INT, PRIMARY KEY (countryName, year))
+`);
+
+// TODO: Create SOCIETALDATA table and define attributes, domains, and constraints
+await db.query(`
+CREATE TABLE SOCIETALDATA
+(countryName VARCHAR(255) NOT NULL, year INT NOT NULL, hdi INT, gni INT, PRIMARY KEY (countryName, year))
+`);
+
+// TODO: Create ENERGYDATA table and define attributes, domains, and constraints
+await db.query(`
+CREATE TABLE ENERGYDATA
+(countryName VARCHAR(255) NOT NULL, year INT NOT NULL, naturalGasEmissions INT, fuelOilEmissions INT, coalEmissions INT, PRIMARY KEY (countryName, year))
 `);
 
 // TODO: Create OCEANDATA table and define attributes, domains, and constraints
