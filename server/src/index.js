ac758e578d978b17e1a7b3c83df7ded6790f40fe
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-03-04 21:52:11 -0800

    added population to index file

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,7 @@ import atmosphericData from './routes/atmosphericDataRoute.js';
 import landData from './routes/landDataRoute.js';
 import societalData from './routes/societalDataRoute.js';
 import energyData from './routes/energyDataRoute.js';
+import { populateAtmosphericData, populateCountryData } from './population/populateDatabase.js';
 
 export const app = express();
 
@@ -32,3 +33,6 @@ const port = process.env.APP_PORT;
 app.listen(port, () => {
   console.log(`running on port ${port}`);
 });
+
+await populateCountryData();
+await populateAtmosphericData();
