7ff4c71d20f50e3f3b26bd670b8bf51bb061c37b
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-03-09 01:31:46 -0800

    created query component using input boxes and slider bars

---
+++ client/src/pages/components/QueryInput/index.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+import InputBox from '../InputBox';
+import { countries } from '@/labels/countrylist';
+import SliderBar from '../SliderBar';
+import {
+  getEntityAttributes,
+  getYears,
+  entities,
+  operatorTypes,
+  queryTypes,
+  getSliderInfo
+} from './helperFunctions.js';
+
+const QueryBoxes = ({
+  entityInput,
+  setEntityInput,
+  queryTypeInput,
+  setQueryTypeInput,
+  attributeInput,
+  setAttributeInput,
+  operatorInput,
+  setOperatorInput,
+  countryName,
+  setCountryName,
+  year,
+  setYear
+}) => {
+  return (
+    <div className="flex flex-row m-2">
+      <InputBox
+        title={'Entity'}
+        data={entities}
+        value={entityInput}
+        setValue={setEntityInput}
+      />
+      <InputBox
+        title={'Query Type'}
+        data={queryTypes}
+        value={queryTypeInput}
+        setValue={setQueryTypeInput}
+      />
+      {queryTypeInput === 'Select One' && entityInput && (
+        <>
+          <InputBox
+            title={'Country Name'}
+            data={countries}
+            value={countryName}
+            setValue={setCountryName}
+          />
+          {entityInput != 'Country' && (
+            <InputBox
+              title={'Year'}
+              data={getYears(entityInput)}
+              value={year}
+              setValue={setYear}
+            />
+          )}
+        </>
+      )}
+      {queryTypeInput === 'Compare' && entityInput && (
+        <>
+          <InputBox
+            title={'Attribute'}
+            data={getEntityAttributes(entityInput)}
+            value={attributeInput}
+            setValue={setAttributeInput}
+          />
+          {entityInput != 'Country' && (
+            <InputBox
+              title={'Year'}
+              data={getYears(entityInput)}
+              value={year}
+              setValue={setYear}
+            />
+          )}
+          <InputBox
+            title={'Operator'}
+            data={operatorTypes}
+            value={operatorInput}
+            setValue={setOperatorInput}
+          />
+          {attributeInput && (
+            <SliderBar
+              min={getSliderInfo(attributeInput).min}
+              max={getSliderInfo(attributeInput).max}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default QueryBoxes;
