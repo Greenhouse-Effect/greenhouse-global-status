56da524fd9c65d14fe765873df1f51ab4805f857
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-03-10 13:40:15 -0800

    Merge branch 'testPopulationHeatMap' of github.com:Greenhouse-Effect/greenhouse-global-status into testPopulationHeatMap
    Co-authored-by: DanielBKurt <DanielBKurt@users.noreply.github.com>

--- client/src/pages/components/MapChart.js
+++ client/src/pages/components/MapChart.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  populationScale,
+  queryByPopulation,
+  populationChangeScale,
+  queryByPopulationChange,
+  populationTooltipInfo
+} from './populationMap';
 import {
   ComposableMap,
   Geographies,
@@ -9,15 +16,15 @@ import {
 
 const geoUrl =
   'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json';
-
 const projConfig = {
   rotate: [-10, 0, 0], // standard rotation, without -10 Russia is split and wraps around to the left
   scale: 100 // 147 is about full screen size
 };
 
 const mapStyle = {
+  // need outline: "none" for all 3, otherwise we get a weird box around countries
   default: {
-    fill: '#D6D6DA',
+    // default fill overrides geography fill, will not allow different colors
     outline: 'none'
   },
   hover: {
@@ -25,16 +32,20 @@ const mapStyle = {
     outline: 'none'
   },
   pressed: {
-    // always leave pressed in even if we make it the same as hover, otherwise it creates weird box around countries when pressed by default
     fill: '#E42',
     outline: 'none'
   }
 };
 
-const MapChart = ({ setToolTipContent }) => {
-  const onMouseEnter = (geo) => {
+const MapChart = ({ setToolTipContent, queryState, queryData }) => {
+  const axios = require('axios');
+  var colorScale = populationScale(queryData);
+
+  const onMouseEnter = (geo, current) => {
     return () => {
-      setToolTipContent(`${geo.properties.name}`);
+      setToolTipContent(
+        `${geo.properties.name}` + `\n` + queryState.tooltipInfo(current)
+      );
     };
   };
 
@@ -42,26 +53,36 @@ const MapChart = ({ setToolTipContent }) => {
     setToolTipContent('');
   };
 
+  // sphere: only sets oval outline around map, does not change shape
+  // graticule: latitude and longitude lines
   return (
     <div>
       <ComposableMap projectionConfig={projConfig}>
-        {/* only sets oval outline around map, does not change shape */}
-        <Sphere stroke="#E4E5E6" strokeWidth={0.5} />{' '}
-        {/* latitude and longitude lines */}
-        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />{' '}
+        <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
         <Geographies geography={geoUrl} data-tooltip-id="my-tooltip">
           {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                onMouseEnter={onMouseEnter(geo)}
-                onMouseLeave={onMouseLeave}
-                style={mapStyle}
-                stroke={'#FFFFFF'} // border color
-                strokeWidth={0.15} // border width (leave very low)
-              />
-            ))
+            geographies.map((geo) => {
+              const current = queryData.find(
+                (s) => s.countryName == geo.properties.name
+              );
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={onMouseEnter(geo, current)}
+                  onMouseLeave={onMouseLeave}
+                  style={mapStyle}
+                  stroke={'#FFFFFF'} // border color
+                  strokeWidth={0.15} // border width (leave very low)
+                  fill={
+                    current
+                      ? queryState.colorScale(queryState.queryBy(current))
+                      : '#D4D4D4'
+                  } // if current (data for country) is not null, find color on scale, otherwise default gray
+                />
+              );
+            })
           }
         </Geographies>
       </ComposableMap>
