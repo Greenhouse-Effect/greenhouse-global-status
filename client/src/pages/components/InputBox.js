7cc718e696ded2a42838e56a64df07295abee156
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-03-10 12:48:34 -0800

    removed fragment

--- client/src/pages/components/InputBox.js
+++ client/src/pages/components/InputBox.js
@@ -4,18 +4,16 @@ import Autocomplete from '@mui/material/Autocomplete';
 
 const InputBox = ({ title, data, value, setValue }) => {
   return (
-    <>
-      <Autocomplete
-        className="p-2"
-        inputValue={value}
-        onInputChange={(event, newInputValue) => {
-          setValue(newInputValue);
-        }}
-        options={data}
-        sx={{ width: 250 }}
-        renderInput={(params) => <TextField {...params} label={title} />}
-      />
-    </>
+    <Autocomplete
+      className="p-2"
+      inputValue={value}
+      onInputChange={(event, newInputValue) => {
+        setValue(newInputValue);
+      }}
+      options={data}
+      sx={{ width: 250 }}
+      renderInput={(params) => <TextField {...params} label={title} />}
+    />
   );
 };
 
