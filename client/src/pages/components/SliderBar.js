1bc6ffc7837d0dd8074ab581bfa04c1f909d69e8
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-03-10 12:48:56 -0800

    removed fragment and updated on change function

--- client/src/pages/components/SliderBar.js
+++ client/src/pages/components/SliderBar.js
@@ -3,23 +3,19 @@ import React, { useState } from 'react';
 import Slider from '@mui/material/Slider';
 
 const SliderBar = ({ min, max, value, setValue }) => {
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   return (
-    <>
-      <Slider
-        className="m-5"
-        defaultValue={max / 2}
-        valueLabelDisplay="auto"
-        min={min}
-        max={max}
-        value={value}
-        onChange={handleChange}
-        track={false}
-      />
-    </>
+    <Slider
+      className="m-5"
+      defaultValue={max / 2}
+      valueLabelDisplay="auto"
+      min={min}
+      max={max}
+      value={value}
+      onChange={(event, newSliderValue) => {
+        setValue(newSliderValue);
+      }}
+      track={false}
+    />
   );
 };
 
