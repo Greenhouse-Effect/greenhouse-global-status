80ceb851ae6f2c32865722b2e1c80fe4f859b76b
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-03-11 18:58:41 -0800

    updated next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   env: {
     SERVER: process.env.SERVER
   }
 };
-
-module.exports = nextConfig;
