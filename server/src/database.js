e50c6e35a6a648ef78c018256b8507107c10139c
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-03-14 18:52:17 -0700

    normalized tables

--- server/src/database.js
+++ server/src/database.js
@@ -2,14 +2,18 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import mysql from 'mysql2';
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-}).promise();
+export const db = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  })
+  .promise();
 
-await db.query(`DROP TABLE IF EXISTS COUNTRY, ATMOSPHERICDATA, LANDDATA, SOCIETALDATA, ENERGYDATA, FOODDATA, DISEASEDATA, DISASTERDATA`)
+await db.query(
+  `DROP TABLE IF EXISTS COUNTRY, ATMOSPHERICDATA, LANDDATA, SOCIETALDATA, ENERGYDATA, FOODDATA, DISEASEDATA, DISASTERDATA`
+);
 
 // creating country entity in database
 await db.query(`
@@ -28,6 +32,7 @@ CREATE TABLE ATMOSPHERICDATA
   emissionsUnit VARCHAR(255),
   tempChange DECIMAL(4, 3), 
   tempUnit VARCHAR(1), 
+  CONSTRAINT AD_Key PRIMARY KEY(countryName, year, emissions, tempChange),
   FOREIGN KEY (countryName) REFERENCES COUNTRY(countryName))
 `);
 
@@ -38,6 +43,7 @@ CREATE TABLE LANDDATA
   year INT NOT NULL, 
   landArea INT, 
   waterWithdrawal INT, 
+  CONSTRAINT LD_Key PRIMARY KEY(countryName, year, landArea, waterWithdrawal),
   FOREIGN KEY (countryName) REFERENCES COUNTRY(countryName))
 `);
 
@@ -48,6 +54,7 @@ CREATE TABLE SOCIETALDATA
   year INT NOT NULL, 
   hdi DECIMAL(4,3), 
   gni INT, 
+  CONSTRAINT SD_Key PRIMARY KEY(countryName, year, hdi, gni),
   FOREIGN KEY (countryName) REFERENCES COUNTRY(countryName))
 `);
 
@@ -59,6 +66,7 @@ CREATE TABLE ENERGYDATA
   naturalGasEmissions DECIMAL(8,3), 
   fuelOilEmissions DECIMAL(8,3), 
   coalEmissions DECIMAL(8,3), 
+  CONSTRAINT SD_Key PRIMARY KEY(countryName, year, naturalGasEmissions, fuelOilEmissions, coalEmissions),
   FOREIGN KEY (countryName) REFERENCES COUNTRY(countryName))
 `);
 
@@ -70,6 +78,7 @@ CREATE TABLE FOODDATA
   riceProduction INT, 
   cornProduction INT, 
   wheatProduction INT, 
+  CONSTRAINT FD_Key PRIMARY KEY(countryName, year, riceProduction, cornProduction, wheatProduction),
   FOREIGN KEY (countryName) REFERENCES COUNTRY(countryName))
 `);
 
@@ -81,6 +90,7 @@ CREATE TABLE DISEASEDATA
   rabiesIncidence INT, 
   malariaIncidence INT, 
   countryInfection DECIMAL(4,2), 
+  CONSTRAINT DISEASE_Key PRIMARY KEY(countryName, year, rabiesIncidence, malariaIncidence, countryInfection),
   FOREIGN KEY (countryName) REFERENCES COUNTRY(countryName))
 `);
 
@@ -92,5 +102,6 @@ CREATE TABLE DISASTERDATA
   deaths INT, 
   homelessness INT, 
   economicDamages INT, 
+  CONSTRAINT DISASTER_Key PRIMARY KEY(countryName, year, deaths, homelessness, economicDamages),
   FOREIGN KEY (countryName) REFERENCES COUNTRY(countryName))
 `);
